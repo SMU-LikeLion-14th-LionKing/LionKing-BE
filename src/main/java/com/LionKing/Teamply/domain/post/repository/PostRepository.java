33b29cdd6b3877523ab1bfa27151b6780e348d0b
kangho <kil0653@naver.com>
2026-07-20 18:58:49 +0900

    ✨feat: 게시글(공지사항) 레포지토리 구현

---
+++ src/main/java/com/LionKing/Teamply/domain/post/repository/PostRepository.java
@@ -0,0 +1,23 @@
+package com.LionKing.Teamply.domain.post.repository;
+
+import com.LionKing.Teamply.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    //이건 공지사항 목록 최신순으로 받아오는 쿼리문입니다.
+    Page<Post> findAllByProject_IdAndTypeOrderByCreatedAtDesc(
+            Long projectId, String type, Pageable pageable
+    );
+
+    //이건 공지사항 목록 최신순으로 3개 받아오는 쿼리문입니당
+    List<Post> findTop3ByProject_IdAndTypeOrderByCreatedAtDesc(
+            Long projectId, String type
+    );
+}
